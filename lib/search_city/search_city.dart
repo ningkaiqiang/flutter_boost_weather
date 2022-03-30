c4e1d370aa168024589f3b8307a1fadb956e6d13
ningkaiqiang <nkq_1998@outlook.com>
2022-03-30 17:27:44 +0800

    添加搜索框

--- lib/search_city/search_city.dart
+++ lib/search_city/search_city.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
+import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_boost/flutter_boost.dart';
 
 class TransparentWidget extends StatefulWidget {
   @override
@@ -10,6 +10,7 @@ class TransparentWidget extends StatefulWidget {
 
 class TransparentWidgetState extends State<TransparentWidget> {
   Color _myColor = Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+  final TextEditingController _controller = new TextEditingController();
 
   @override
   void initState() {
@@ -19,58 +20,75 @@ class TransparentWidgetState extends State<TransparentWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0x00000000),
+        backgroundColor: const Color(0xffffffff),
         body: Container(
-          alignment: Alignment.bottomCenter,
+          alignment: Alignment.topCenter,
+          margin: const EdgeInsets.only(top: 50),
           child: Container(
             decoration: BoxDecoration(
                 color: const Color(0xffffffff),
                 border: Border.all(color: _myColor, width: 1.0)),
-            height: 300,
+            height: 700,
+            width: 350,
             child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Expanded(
-                      flex: 1,
-                      child: Container(
-                        margin: const EdgeInsets.all(24.0),
-                        child: Text(
-                          'This is translucent dialog.',
-                          style: TextStyle(fontSize: 22.0, color: _myColor),
+                  Text(
+                      '城市搜索',
+                      style: TextStyle(
+                          fontSize: 20, color: Colors.black
+                      )
+                  ),
+                  Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                            color: const Color(0xffffffff),
+                            border: Border.all(color: _myColor, width: 1.0)),
+                        margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+                        child: TextField(
+                          style: TextStyle(
+                            fontSize: 17,
+                          ),
+                          controller: _controller,
+                          decoration: new InputDecoration(
+                            hintText: '输入要搜索的城市吧',
+                            hintStyle: new TextStyle(
+                              fontSize: 17
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(20),
+                              ),
+                            ),
+                            contentPadding: EdgeInsets.only(
+                              top: 0,
+                              bottom: 0,
+                            ),
+                            prefixIcon: Icon(Icons.saved_search_rounded),
+                          ),
                         ),
-                      )),
+                      ),
+                    ],
+                  ),
                   Expanded(
                     flex: 1,
                     child: Column(
                         mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.end,
                         children: <Widget>[
                           InkWell(
                             child: Container(
                                 padding: const EdgeInsets.all(8.0),
                                 margin: const EdgeInsets.all(8.0),
-                                color: Colors.yellow,
+                                color: const Color(0xfff36091),
                                 child: Text(
                                   'Go Back',
                                   style: TextStyle(
-                                      fontSize: 22.0, color: Colors.black),
+                                      fontSize: 17.0, color: Colors.black),
                                 )),
                             onTap: () => Navigator.of(context).pop(),
-                          ),
-                          InkWell(
-                            child: Container(
-                                padding: const EdgeInsets.all(8.0),
-                                margin: const EdgeInsets.all(8.0),
-                                color: Colors.yellow,
-                                child: Text(
-                                  'Open New Dialog',
-                                  style: TextStyle(
-                                      fontSize: 22.0, color: Colors.black),
-                                )),
-                            onTap: () => BoostNavigator.instance.push(
-                                "transparentWidget",
-                                withContainer: true,
-                                opaque: false),
                           ),
                         ]),
                   ),
