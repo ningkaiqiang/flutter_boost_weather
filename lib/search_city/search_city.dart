2cd8e5dda78f35ea1578422d1a88ac680a1d447b
ningkaiqiang <nkq_1998@outlook.com>
2022-03-31 17:49:56 +0800

    UI调整

--- lib/search_city/search_city.dart
+++ lib/search_city/search_city.dart
@@ -23,77 +23,63 @@ class TransparentWidgetState extends State<TransparentWidget> {
         backgroundColor: const Color(0xffffffff),
         body: Container(
           alignment: Alignment.topCenter,
-          margin: const EdgeInsets.only(top: 50),
-          child: Container(
-            decoration: BoxDecoration(
-                color: const Color(0xffffffff),
-                border: Border.all(color: _myColor, width: 1.0)),
-            height: 700,
-            width: 350,
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                      '城市搜索',
-                      style: TextStyle(
-                          fontSize: 20, color: Colors.black
-                      )
+          margin: const EdgeInsets.only(top: 50, left: 10, right: 10),
+          height: 700,
+          // decoration: new BoxDecoration(
+          //     border: Border.all(color: Colors.black, width: 1.0)
+          // ),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.topLeft,
+                  margin: const EdgeInsets.only(left: 20, top: 40),
+                  child: Text(
+                    '城市搜索',
+                    style: TextStyle(
+                        fontSize: 30, color: Colors.black
+                    ),
+                    textAlign: TextAlign.left,
                   ),
-                  Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        decoration: BoxDecoration(
-                            color: const Color(0xffffffff),
-                            border: Border.all(color: _myColor, width: 1.0)),
-                        margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
-                        child: TextField(
-                          style: TextStyle(
-                            fontSize: 17,
-                          ),
-                          controller: _controller,
-                          decoration: new InputDecoration(
-                            hintText: '输入要搜索的城市吧',
-                            hintStyle: new TextStyle(
-                              fontSize: 17
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(20),
-                              ),
-                            ),
-                            contentPadding: EdgeInsets.only(
-                              top: 0,
-                              bottom: 0,
-                            ),
-                            prefixIcon: Icon(Icons.saved_search_rounded),
-                          ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+                  child: TextField(
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                    controller: _controller,
+                    decoration: new InputDecoration(
+                      hintText: '输入要搜索的城市吧',
+                      hintStyle: new TextStyle(
+                          fontSize: 17
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
                         ),
                       ),
-                    ],
+                      contentPadding: EdgeInsets.only(
+                        top: 0,
+                        bottom: 0,
+                      ),
+                      prefixIcon: Icon(Icons.saved_search_rounded),
+                    ),
                   ),
-                  Expanded(
-                    flex: 1,
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: <Widget>[
-                          InkWell(
-                            child: Container(
-                                padding: const EdgeInsets.all(8.0),
-                                margin: const EdgeInsets.all(8.0),
-                                color: const Color(0xfff36091),
-                                child: Text(
-                                  'Go Back',
-                                  style: TextStyle(
-                                      fontSize: 17.0, color: Colors.black),
-                                )),
-                            onTap: () => Navigator.of(context).pop(),
-                          ),
-                        ]),
+                ),
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  margin: const EdgeInsets.only(top: 20),
+                  child: InkWell(
+                    child: Icon(
+                      Icons.cancel,
+                      size: 40,
+                      color: _myColor,
+                    ),
+                    onTap: () => Navigator.of(context).pop(),
                   ),
-                ]),
-          ),
+                ),
+              ]),
         ));
   }
 }
